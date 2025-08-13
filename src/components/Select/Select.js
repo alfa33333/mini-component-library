580069b9cd3c7719c0574652f8f426c4c8a4ca15
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-08-12 23:29:31 -0600

    Refactor Select component to include a wrapper for improved styling and display of selected value

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -8,11 +8,49 @@ import { getDisplayedValue } from './Select.helpers';
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+  return ( 
+      <Wrapper>
+        <p>{displayedValue}</p>
+        <select value={value} onChange={onChange}>
+          {children}
+        </select>
+      </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+    background-color: ${COLORS.transparentGray15};
+    color: ${COLORS.gray700};
+    font-size: 16px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    border-radius: 8px;
+    border: none;
+    padding: 12px 16px; 
+
+    & p{
+      user-select: none;
+    }
+
+    select {
+      opacity: 0;
+      background-color: ${COLORS.transparentGray15};
+      position: absolute;
+      top: 0;
+      left: 0;
+      font-size: 16px;
+    }
+
+    &:focus-within {
+     outline: 2px solid ${COLORS.primary};
+    }
+`;
+
+
+
+
 
 export default Select;
