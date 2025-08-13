9f9865d8c9917a1cd59238d77d4b7c1f02872322
Antonio Herrera Martin <44150484+alfa33333@users.noreply.github.com>
2025-08-13 16:23:08 -0600

    Implement IconInput component with styled input and icon integration

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,19 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZE = {
+  small: {
+    "--height": 24,
+    "--indent": 32,
+    "--font-size": 14,
+  },
+  large: {
+    "--height": 36,
+    "--indent": 48,
+    "--font-size": 18,
+  },
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +26,49 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+
+  return <Wrapper >
+    <VisuallyHidden>
+      <label >{label}</label>
+    </VisuallyHidden>
+    <StyledInput width={width} placeholder={placeholder} style={SIZE[size]}/>
+    <PlaceIcon id={icon} size={SIZE[size]["--height"]} />
+  </Wrapper>;
 };
+
+const PlaceIcon = styled(Icon)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: ${COLORS.gray500};
+  pointer-events: none;
+`;
+
+const StyledInput = styled.input`
+  width: ${props => props.width + 'px'};
+  height: calc(var(--height)*1px);  
+  border: none;
+  border-bottom: 1px solid ${COLORS.black};
+  font-size: calc(var(--font-size)*1px);
+  font-weight: 700;
+  text-indent: calc(var(--indent)*1px);
+  color: ${COLORS.gray700};
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+  align-items: center;
+  &:hover{
+    ${StyledInput}, ${PlaceIcon} {
+      color: ${COLORS.black};
+    }
+  }
+`;
 
 export default IconInput;
